9506e37b6f14809307f9954de8ccf891c37e711c
Robert Konarskis <r.konarskis@tmg.nl>
2019-05-06 00:15:13 +0200

    cleanup

--- list/src/main/java/com/konarskirob/list/ListFeature.kt
+++
@@ -1,9 +0,0 @@
-package com.konarskirob.list
-
-import android.content.Context
-import android.content.Intent
-
-class ListFeature(private val context: Context) {
-
-    fun getIntent(): Intent = ListActivity.newIntent(context)
-}
