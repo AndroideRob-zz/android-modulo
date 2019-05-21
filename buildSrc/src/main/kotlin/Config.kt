9855c4d07a030cc6a99ac21a6fa7bab3661041c9
Robert Konarskis <r.konarskis@tmg.nl>
2019-05-21 16:51:45 +0200

    wip

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,46 @@
+import org.gradle.api.JavaVersion
+
+object Config {
+
+    val minSdk = 21
+    val compileSdk = 28
+    val targetSdk = 28
+    val javaVersion = JavaVersion.VERSION_1_8
+    val buildTools = "28.0.3"
+}
+
+object Versions {
+
+    val androidx_core = "1.0.1"
+    val androidx_recyclerview = "1.0.0"
+    val androidx_navigation = "2.0.0"
+    val androidx_constraintLayout = "1.1.3"
+    val material = "1.1.0-alpha04"
+
+    val junit = "4.12"
+    val androidx_espresso = "3.1.0"
+    val androidx_testing = "1.1.1"
+
+    val gradleandroid = "3.4.1"
+    val kotlin = "1.3.21"
+}
+
+object Deps {
+
+    val androidx_core = "androidx.core:core-ktx:${Versions.androidx_core}"
+    val androidx_constraintlayout = "androidx.constraintlayout:constraintlayout:${Versions.androidx_constraintLayout}"
+    val androidx_material = "com.google.android.material:material:${Versions.material}"
+    val androidx_navigation_fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.androidx_navigation}"
+    val androidx_navigation_ui = "androidx.navigation:navigation-ui-ktx:${Versions.androidx_navigation}"
+    val androidx_recyclerview = "androidx.recyclerview:recyclerview:${Versions.androidx_recyclerview}"
+
+    val testlib_junit = "junit:junit:${Versions.junit}"
+
+    val testandroidx_rules = "androidx.test:rules:${Versions.androidx_testing}"
+    val testandroidx_runner = "androidx.test:runner:${Versions.androidx_testing}"
+    val testandroidx_espressocore = "androidx.test.espresso:espresso-core:${Versions.androidx_espresso}"
+
+    val tools_gradleandroid = "com.android.tools.build:gradle:${Versions.gradleandroid}"
+    val tools_kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+}
+
--- core/src/main/java/com/konarskirob/core/CallbackFragment.kt
+++ core/src/main/java/com/konarskirob/core/CallbackFragment.kt
@@ -6,5 +6,13 @@ abstract class CallbackFragment<I, T> : Fragment() {
 
     abstract var input: I?
 
-    abstract var callback: (T) -> Unit
+    abstract var callback: ((T) -> Unit)?
+
+    /**
+     * Resets references to avoid potential memory leaks.
+     */
+    open fun clear() {
+        input = null
+        callback = null
+    }
 }
